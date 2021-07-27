e4972dffdedc8c74ef43d3646d43325ad476f2b8
Fernando Medeiros <79485966+fernandomeddev@users.noreply.github.com>
2021-07-27 00:31:51 -0300

    end poins Get e Post concluidos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const express =require('express')
+const express = require('express')
 const app = express()
+app.use(express.json());
 
 const db = require('./models')
 
+//Rotas
+const taskRouter = require('./routes/Tasks')
+app.use("/tasks", taskRouter);
 
-db.sequelize.sync().then(()=>{
+db.sequelize.sync().then(()=>{ 
     app.listen(3001,()=>{
         console.log('server running on port 3001');
     });
